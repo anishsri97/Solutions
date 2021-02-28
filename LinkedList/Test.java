65ad9f06aa47b166825aacba6e6d55e525596bf3
Anish Srivastava <siranish97@gmail.com>
2021-02-28 15:57:18 +0530

    done

---
+++ LinkedList/Test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import DLinkedList.*;
+public class Test 
+{
+   public static void main(String args[])
+   {
+     LinkedList list = new LinkedList();
+     list.add(23);
+    
+   }    
+}
