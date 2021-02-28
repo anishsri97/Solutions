a255217e7a8cd8584fe52f9e063c9c4f835034cf
Anish Srivastava <siranish97@gmail.com>
2021-02-28 15:45:12 +0530

    code changes

---
+++ LinkedList/Node.java
@@ -0,0 +1,13 @@
+package Data-Structures-and-Algorithms.LinkedList;
+
+public class Node 
+{
+   int data;
+   Node next;
+   
+   Node(int data)
+   {
+       this.data=data;
+       next=null;
+   }
+}
